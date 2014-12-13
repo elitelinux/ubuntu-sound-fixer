9829567fec758012aece09a44b96dd9305197b3f
Juan Fernando Villa Hernández <elite.linux@gmail.com>
2014-12-13 08:22:48 -0500

    Update fixer.sh

--- fixer.sh
+++ fixer.sh
@@ -1,1 +1,4 @@
 sudo apt-get update;sudo apt-get dist-upgrade; sudo apt-get install pavucontrol linux-sound-base alsa-base alsa-utils lightdm ubuntu-desktop  linux-image-`uname -r` libasound2 libasound2-dev libasound2-plugins-extra; sudo apt-get -y --reinstall install linux-sound-base alsa-base alsa-utils lightdm ubuntu-desktop  linux-image-`uname -r` libasound2 ; killall pulseaudio; rm -r ~/.pulse*; ubuntu-support-status; sudo usermod -aG `cat /etc/group | grep -e '^pulse:' -e '^audio:' -e '^pulse-access:' -e '^pulse-rt:' -e '^video:' | awk -F: '{print $1}' | tr '\n' ',' | sed 's:,$::g'` `whoami`
+wget -O alsa-info.sh http://www.alsa-project.org/alsa-info.sh && chmod +x ./alsa-info.sh && ./alsa-info.sh
+sudo apt-get remove --purge alsa-base pulseaudio && sudo apt-get install alsa-base pulseaudio && sudo alsa force-reload
+sudo usermod -aG audio,pulse,pulse-access frew
